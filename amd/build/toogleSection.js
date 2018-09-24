851d743aeec10ac5c9ff23d35927d7004bfe9dd6
ilya dymshits <ilya@devlion.co>
2018-09-24 23:48:46 +0300

    (update) add ajax to send new section numbers

--- amd/build/toogleSection.js
+++ amd/build/toogleSection.js
@@ -1,6 +1,7 @@
 define(['jquery'], function($) {
 'use strict';
 
+const MAINCLASS = `format-stardust`;
 const mainBlock = document.querySelector(`.format-stardust .card`);
 
 const toggleSection = (target) => {
@@ -16,13 +17,46 @@ const toggleAllSection = (target) => {
     $(target).removeClass(`section_opened`);
     $('.flexsections-level-0 .section.img-text').slideUp(1000);
     $('.flexsections-level-0 .flexsections-level-2').slideUp(1000);
+    $('.flexsections-level-0 .flexsections-level-3').slideUp(1000);
   }else {
     $('.flexsections-level-0 .section_wrap').addClass(`section-opened`);
     $(target).addClass(`section_opened`);
     $('.flexsections-level-0 .section.img-text').slideDown(1000);
     $('.flexsections-level-0 .flexsections-level-2').slideDown(1000);
+    $('.flexsections-level-0 .flexsections-level-3').slideDown(1000);
   }
 
+}
+
+// set section numbers
+const sectionsnumber = document.querySelector(`#page-course-view-stardust.format-stardust.editing`);
+const sections = mainBlock.querySelectorAll(`.section.main`);
+const sendAjex = () => {
+
+  let sectionNumbers = [];
+  let section = [];
+  sections.forEach((item)=>{
+    let fakenumber = item.querySelector('.sectionnumber').innerHTML ? item.querySelector('.sectionnumber').innerHTML : 0;
+    let sectionid = item.id.replace(/\D+/, '');
+    section = [
+      sectionid,
+      fakenumber
+    ];
+    if (sectionid && fakenumber) sectionNumbers.push(section);
+
+  });
+  sectionNumbers = JSON.stringify(sectionNumbers);
+
+  $.ajax({
+      type: "POST",
+      async: true,
+      url: M.cfg.wwwroot + '/theme/stardust/mypublic-ajax.php',
+      data: sectionNumbers,
+      success: function(data) {
+      },
+      error: function(requestObject, error, errorThrown) {
+      }
+  });
 }
 
     return {
@@ -47,6 +81,21 @@ const toggleAllSection = (target) => {
               target = target.parentNode;
             }
           });
+
+          if (sectionsnumber) {
+            sectionsnumber.addEventListener('click', function(e){
+              let target = e.target;
+              while (!target.classList.contains(MAINCLASS)) {
+
+                if (target.classList.contains(`movehere`)) {
+                  sendAjex();console.dir('send');
+                  return
+                }
+
+              target = target.parentNode;
+              }
+            });
+          }
 
         }
     };
