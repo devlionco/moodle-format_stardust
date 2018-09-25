c57f36f5c2b0779a37d85c6bd613f84815de1d3f
ilya dymshits <ilya@devlion.co>
2018-09-26 00:18:18 +0300

    (update) remove ajax from section numbers

--- amd/src/toggleSection.js
+++ amd/src/toggleSection.js
@@ -28,37 +28,6 @@ const toggleAllSection = (target) => {
 
 }
 
-// set section numbers
-const sectionsnumber = document.querySelector(`#page-course-view-stardust.format-stardust.editing`);
-const sections = mainBlock.querySelectorAll(`.section.main`);
-const sendAjex = () => {
-
-  let sectionNumbers = [];
-  let section = [];
-  sections.forEach((item)=>{
-    let fakenumber = item.querySelector('.sectionnumber').innerHTML ? item.querySelector('.sectionnumber').innerHTML : 0;
-    let sectionid = item.id.replace(/\D+/, '');
-    section = [
-      sectionid,
-      fakenumber
-    ];
-    if (sectionid && fakenumber) sectionNumbers.push(section);
-
-  });
-  sectionNumbers = JSON.stringify(sectionNumbers);
-
-  $.ajax({
-      type: "POST",
-      async: true,
-      url: M.cfg.wwwroot + '/theme/stardust/mypublic-ajax.php',
-      data: sectionNumbers,
-      success: function(data) {
-      },
-      error: function(requestObject, error, errorThrown) {
-      }
-  });
-}
-
     return {
         init: function() {
 
@@ -82,23 +51,9 @@ const sendAjex = () => {
             }
           });
 
+          // open section on single page
           if(window.location.href.indexOf('&sectionid') > -1) {
              $('.sectiontoggle').trigger( "click" );
-          }
-
-          if (sectionsnumber) {
-            sectionsnumber.addEventListener('click', function(e){
-              let target = e.target;
-              while (!target.classList.contains(MAINCLASS)) {
-
-                if (target.classList.contains(`movehere`)) {
-                  sendAjex();
-                  return
-                }
-
-              target = target.parentNode;
-              }
-            });
           }
 
         }
